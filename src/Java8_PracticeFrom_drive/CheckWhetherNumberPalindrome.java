f4913a629eb9bf6d54be8f8522a819bbfe714a32
Idd mohammad <mohammadidd345@gmail.com>
2025-04-19 16:13:15 +0530

    Java Program to Check Whether a Number is Palindrome or Not

--- src/Java8_PracticeFrom_drive/CheckWhetherNumberPalindrome.java
+++ src/Java8_PracticeFrom_drive/CheckWhetherNumberPalindrome.java
@@ -1,2 +1,18 @@
-package Java8_PracticeFrom_drive;public class CheckWhetherNumberPalindrome {
+package Java8_PracticeFrom_drive;
+
+public class CheckWhetherNumberPalindrome {
+    public static void main(String[] args) {
+        int num=121,n,rev=0;
+        int st=num;
+        while (num>0){
+            n=num%10;
+            rev=rev*10+n;
+            num=num/10;
+        }
+        if (st==rev){
+            System.out.println("number is palindrome..");
+        }else {
+            System.out.println("number is not palindrome..");
+        }
+    }
 }
--- src/Java8_PracticeFrom_drive/SwapTwoNumbersWithoutUsingThirdVariable.java
+++ src/Java8_PracticeFrom_drive/SwapTwoNumbersWithoutUsingThirdVariable.java
@@ -1,2 +1,16 @@
-package Java8_PracticeFrom_drive;public class SwapTwoNumbersWithoutUsingThirdVariable {
+package Java8_PracticeFrom_drive;
+
+public class SwapTwoNumbersWithoutUsingThirdVariable {
+    public static void main(String[] args) {
+        int a =7;
+        int b=8;
+
+        a=a+b;//a=15
+       b= a-b;//b=7
+       a=a-b;//a=8
+
+        System.out.println(" a = "+a+
+                           "\n b = "+b);
+
+    }
 }
